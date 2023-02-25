6ada82c6c58a03d55bc114309819787d61ab6202
why-002 <115664954+why-002@users.noreply.github.com>
2023-02-25 13:33:09 -0600

    Added filler to the constructor to take care of cases of less than 16 bytes

--- src/Matrix.java
+++ src/Matrix.java
@@ -7,6 +7,22 @@ public class Matrix {
         this.rows = rows;
         this.columns = columns;
         int positionCount = 0;
+
+        if (byteString.length < 16){
+            byte byteFillerAmount = (byte) ((byte) 16-byteString.length);
+            byte[] newByteString = new byte[16];
+            for (byte fillerIndex = 0; fillerIndex < newByteString.length; fillerIndex++){
+                if (fillerIndex < byteString.length){
+                    newByteString[fillerIndex] = byteString[fillerIndex];
+                }
+
+                else{
+                    newByteString[fillerIndex] = byteFillerAmount;
+                }
+            }
+            byteString = newByteString;
+        }
+
         for (int i=0;i<rows;i++){
             for (int j=0;j<columns;j++){
                 matrix[j][i] = intString[positionCount];
